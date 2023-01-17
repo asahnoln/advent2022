2b546cae73ad4c6624a503eab6185530f738353a
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-01-18 04:12:00 +0600

    Readme and docs
    - [ADDED] Readme and docs info

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,17 @@ module Lib (findLargestCalory, convertSringToCalories) where
 
 import Data.List (groupBy)
 
+{- | `findLargestCalory` finds largest calory sum from the list of calories
+>>> findLargestCalory [[1, 2, 3], [5, 6], [4, 1]]
+11 -- From the second list [5, 6]
+-}
 findLargestCalory :: [[Integer]] -> Integer
 findLargestCalory xss = maximum $ map sum xss
 
+{- | `convertSringToCalories` converts given strings with newline to list of calories.
+>>> convertSringToCalories "1\\n2\\n3\\n\\n4\\n\\n5\\n6"
+[[1, 2, 3], [4], [5, 6]]
+-}
 convertSringToCalories :: String -> [[Integer]]
 convertSringToCalories xs = map (map read) ys
   where
