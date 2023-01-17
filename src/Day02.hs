8c925f410afc7d40bd505cd4e4638061b34a5c6f
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-01-18 05:57:21 +0600

    Basic data for RSP
    - [ADDED] Data to work with
    - [CHANGED] Code formattin

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,1 +1,55 @@
-module Day02 () where
+module Day02 (
+    match,
+    score,
+    Hand (..),
+    hand,
+    Result (..),
+    scoreMatch,
+)
+where
+
+data Hand = Wrong | Paper | Scissors | Rock deriving (Eq, Show)
+
+instance Ord Hand where
+    Paper `compare` Paper = EQ
+    Rock `compare` Rock = EQ
+    Scissors `compare` Scissors = EQ
+    Wrong `compare` Wrong = EQ
+    Paper `compare` Rock = GT
+    Rock `compare` Scissors = GT
+    Scissors `compare` Paper = GT
+    _ `compare` Wrong = GT
+    Rock `compare` Paper = LT
+    Scissors `compare` Rock = LT
+    Paper `compare` Scissors = LT
+    Wrong `compare` _ = LT
+
+data Result = Lose | Draw | Win
+
+hand :: Char -> Hand
+hand 'A' = Rock
+hand 'B' = Scissors
+hand 'C' = Paper
+hand 'X' = Rock
+hand 'Y' = Scissors
+hand 'Z' = Paper
+hand _ = Wrong
+
+score :: Hand -> Integer
+score Rock = 1
+score Paper = 2
+score Scissors = 3
+score _ = 0
+
+scoreMatch :: Result -> Integer
+scoreMatch Win = 6
+scoreMatch Draw = 3
+scoreMatch Lose = 0
+
+match :: Hand -> Hand -> Integer
+match x y = score y + scoreMatch result
+  where
+    result = case compare y x of
+        GT -> Win
+        EQ -> Draw
+        LT -> Lose
--- test/Spec02.hs
+++ test/Spec02.hs
@@ -1,9 +1,41 @@
 module Spec02 (spec) where
 
-import Day02 ()
+import Day02 (
+    Hand (..),
+    Result (..),
+    hand,
+    match,
+    score,
+    scoreMatch,
+ )
 import Test.Hspec
 
 spec :: SpecWith ()
 spec = describe "Day 2 elves" $ do
-    it "can find biggest calory" $ do
-        True `shouldBe` True
+    context "play Rock Scissors Paper where" $ do
+        it "basic RSP rules work" $ do
+            compare Rock Scissors `shouldBe` GT
+            compare Paper Scissors `shouldBe` LT
+            compare Paper Paper `shouldBe` EQ
+            compare Paper Rock `shouldBe` GT
+        it "letters to hands" $ do
+            hand 'A' `shouldBe` Rock
+            hand 'B' `shouldBe` Scissors
+            hand 'C' `shouldBe` Paper
+            hand 'X' `shouldBe` Rock
+            hand 'Y' `shouldBe` Scissors
+            hand 'Z' `shouldBe` Paper
+            hand '0' `shouldBe` Wrong
+        it "scores from hands" $ do
+            score Rock `shouldBe` 1
+            score Paper `shouldBe` 2
+            score Scissors `shouldBe` 3
+            score Wrong `shouldBe` 0
+        it "scores from matches" $ do
+            scoreMatch Win `shouldBe` 6
+            scoreMatch Draw `shouldBe` 3
+            scoreMatch Lose `shouldBe` 0
+        it "I get score with rock against rock" $ do
+            match Rock Paper `shouldBe` 8
+            match Paper Rock `shouldBe` 1
+            match Scissors Scissors `shouldBe` 6
