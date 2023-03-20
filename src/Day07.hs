0825762f467c1900fb9ec308b4d649c55fc731d2
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-03-21 00:18:14 +0600

    feat(day07): total function for calculating dir sizes (not finished)

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,4 +1,50 @@
-module Day07 where
+module Day07 (total) where
 
 total :: String -> Integer
-total x = undefined
+total x = result
+  where
+    cmds =
+        "$ cd a\n\
+        \$ ls\n\
+        \dir e\n\
+        \29116 f\n\
+        \2557 g\n\
+        \62596 h.lst\n\
+        \$ cd e\n\
+        \$ ls\n\
+        \584 i\n"
+    e =
+        calcDir
+            "584 i"
+    a =
+        calcDir
+            "dir e\n\
+            \29116 f\n\
+            \2557 g\n\
+            \62596 h.lst\n"
+
+    result = parseCmds cmds
+
+    parseCmds :: String -> Integer
+    parseCmds ss = undefined -- sum [ a , e , e ]
+    getSize :: String -> Integer
+    getSize ss = r
+      where
+        info = head $ words ss
+        r = if info == "dir" then 0 else read info
+
+    calcDir :: String -> Integer
+    calcDir cs = r
+      where
+        fs = lines cs
+        r = sum $ map getSize fs
+
+-- parseCmds :: String -> Integer
+-- parseCmds cs = result
+--   where
+--     (_ : _ : fs) = lines cs
+--     result =
+--         sum
+--             [ sum $ map getSize $ tail fs
+--             , calcDir $ unlines ["cd", "ls", head fs]
+--             ]
--- test/Spec07.hs
+++ test/Spec07.hs
@@ -9,27 +9,27 @@ spec = do
         context "launch commands" $ do
             it "finds the total of all directories of size at most 100000" $ do
                 total
-                    "$ cd /\
-                    \$ ls\
-                    \dir a\
-                    \14848514 b.txt\
-                    \8504156 c.dat\
-                    \dir d\
-                    \$ cd a\
-                    \$ ls\
-                    \dir e\
-                    \29116 f\
-                    \2557 g\
-                    \62596 h.lst\
-                    \$ cd e\
-                    \$ ls\
-                    \584 i\
-                    \$ cd ..\
-                    \$ cd ..\
-                    \$ cd d\
-                    \$ ls\
-                    \4060174 j\
-                    \8033020 d.log\
-                    \5626152 d.ext\
+                    "$ cd /\n\
+                    \$ ls\n\
+                    \dir a\n\
+                    \14848514 b.txt\n\
+                    \8504156 c.dat\n\
+                    \dir d\n\
+                    \$ cd a\n\
+                    \$ ls\n\
+                    \dir e\n\
+                    \29116 f\n\
+                    \2557 g\n\
+                    \62596 h.lst\n\
+                    \$ cd e\n\
+                    \$ ls\n\
+                    \584 i\n\
+                    \$ cd ..\n\
+                    \$ cd ..\n\
+                    \$ cd d\n\
+                    \$ ls\n\
+                    \4060174 j\n\
+                    \8033020 d.log\n\
+                    \5626152 d.ext\n\
                     \7214296 k"
                     `shouldBe` 95437
