e1a151e2cf31d374470a2f5a518c821dcbbe8a8f
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-04-02 02:40:48 +0600

    test(day07): removed all code and started small

--- test/Spec07.hs
+++ test/Spec07.hs
@@ -8,28 +8,36 @@ spec = do
     describe "Day 7 elves" $ do
         context "launch commands" $ do
             it "finds the total of all directories of size at most 100000" $ do
+                -- There are dirs with dirs inside only
+                -- There are no empty dirs
                 total
                     "$ cd /\n\
                     \$ ls\n\
-                    \dir a\n\
-                    \14848514 b.txt\n\
-                    \8504156 c.dat\n\
-                    \dir d\n\
-                    \$ cd a\n\
-                    \$ ls\n\
-                    \dir e\n\
-                    \29116 f\n\
-                    \2557 g\n\
-                    \62596 h.lst\n\
-                    \$ cd e\n\
-                    \$ ls\n\
-                    \584 i\n\
-                    \$ cd ..\n\
-                    \$ cd ..\n\
-                    \$ cd d\n\
-                    \$ ls\n\
-                    \4060174 j\n\
-                    \8033020 d.log\n\
-                    \5626152 d.ext\n\
-                    \7214296 k"
-                    `shouldBe` 95437
+                    \100 b.txt"
+                    `shouldBe` 100
+
+-- total
+--     "$ cd /\n\
+--     \$ ls\n\
+--     \dir a\n\
+--     \14848514 b.txt\n\
+--     \8504156 c.dat\n\
+--     \dir d\n\
+--     \$ cd a\n\
+--     \$ ls\n\
+--     \dir e\n\
+--     \29116 f\n\
+--     \2557 g\n\
+--     \62596 h.lst\n\
+--     \$ cd e\n\
+--     \$ ls\n\
+--     \584 i\n\
+--     \$ cd ..\n\
+--     \$ cd ..\n\
+--     \$ cd d\n\
+--     \$ ls\n\
+--     \4060174 j\n\
+--     \8033020 d.log\n\
+--     \5626152 d.ext\n\
+--     \7214296 k"
+--     `shouldBe` 95437
